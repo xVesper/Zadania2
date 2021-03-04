acba7a79f9a1418416edf145a085656773919b04
xVesper <florian.jasiuczenia@gmail.com>
2021-03-04 11:17:10 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+#Zad1
+sport = ["pilka nozna", "siatkowka", "koszykowka"]
+
+sport.reverse()
+print(sport)
+
+sport.append("golf")
+sport.append("tenis")
+print(sport)
+przerwa = "--------------------------------------"
+print(przerwa)
+#Zad2
+slownik = {"itp": "i tym podobne", "itd": "i tak dalej"}
+
+print(slownik)
+print(przerwa)
+#Zad3
+gry = {1: "Borderlands 3", 2: "Battlefield 3", 3: "Borderlands 2"}
+print(gry)
+print(przerwa)
+#Zad4
+napis = input("Napisz zdanie: ")
+
+print(napis.count("a"))
+print(przerwa)
+#Zad5
+
